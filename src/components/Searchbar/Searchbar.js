b0f201e623b5298a2bab21cee06dea26a6135086
Nikita Nikitenko <84923367+Nikita856294@users.noreply.github.com>
2022-06-07 12:35:46 +0300

    refactor Searchbar

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,44 +1,35 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { SearchForm, SubmitButton, InputForm } from './SearchbarStyled';
 import PropTypes from 'prop-types';
 
-class Searchbar extends Component {
-  state = {
-    image: '',
+const Searchbar = ({ onSubmit }) => {
+  const [image, setImage] = useState('');
+  const handleChange = e => {
+    setImage(e.target.value.toLowerCase());
   };
-  handleChange = e => {
-    this.setState({
-      image: e.target.value.toLowerCase(),
-    });
-  };
-  handleSubmit = e => {
-    const { onSubmit } = this.props;
+  const handleSubmit = e => {
     e.preventDefault();
-    onSubmit(this.state.image);
-    this.setState({
-      image: '',
-    });
+    onSubmit(image);
+
+    setImage(image);
   };
 
-  render() {
-    const { image } = this.state;
-    return (
-      <SearchForm className="form" onSubmit={this.handleSubmit}>
-        <SubmitButton type="submit" className="button">
-          <span className="button-label">Search</span>
-        </SubmitButton>
+  return (
+    <SearchForm className="form" onSubmit={handleSubmit}>
+      <SubmitButton type="submit" className="button">
+        <span className="button-label">Search</span>
+      </SubmitButton>
 
-        <InputForm
-          className="input"
-          type="text"
-          value={image}
-          placeholder="Search images and photos"
-          onChange={this.handleChange}
-        />
-      </SearchForm>
-    );
-  }
-}
+      <InputForm
+        className="input"
+        type="text"
+        value={image}
+        placeholder="Search images and photos"
+        onChange={handleChange}
+      />
+    </SearchForm>
+  );
+};
 
 export default Searchbar;
 
