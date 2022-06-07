797894a85e9e189732f678ad6ff1621e401f808a
Nikita Nikitenko <84923367+Nikita856294@users.noreply.github.com>
2022-06-07 12:41:39 +0300

    refactor App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,40 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
 import Container from './Container';
 import { GlobalStyled } from './GlobalStyle/GlobalStyled';
 import { ToastContainer, toast } from 'react-toastify';
 
-export class App extends Component {
-  state = {
-    image: '',
-  };
+export const App = () => {
+  const [image, setImage] = useState('');
 
-  handleSearchFormSubmit = image => {
+  const handleSearchFormSubmit = image => {
     if (image === '') {
       return toast.error('Введите запрос');
     }
-    this.setState({
-      image,
-    });
+    setImage(image);
   };
 
-  render() {
-    const { image } = this.state;
+  return (
+    <div>
+      <GlobalStyled />
+      <Container>
+        <ToastContainer
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
 
-    return (
-      <div>
-        <GlobalStyled />
-        <Container>
-          <ToastContainer
-            autoClose={3000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-          />
-
-          <Searchbar onSubmit={this.handleSearchFormSubmit} />
-          <ImageGallery image={image} />
-        </Container>
-      </div>
-    );
-  }
-}
+        <Searchbar onSubmit={handleSearchFormSubmit} />
+        <ImageGallery image={image} />
+      </Container>
+    </div>
+  );
+};
 
 export default App;
